884628837423764d0271a26ed0c5f6ea34124195
Profesor41 <149595806+Profesor41@users.noreply.github.com>
2025-02-04 19:40:50 +0200

    We add registration

--- app/src/main/java/com/example/registrationpage/MainActivity.kt
+++ app/src/main/java/com/example/registrationpage/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.registrationpage
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,13 +12,33 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-            val userLogin: EditText
+
+        val userLogin: EditText = findViewById(R.id.user_login)
+        val userEmail: EditText = findViewById(R.id.user_email)
+        val userPass: EditText = findViewById(R.id.user_pass)
+        val button: Button = findViewById(R.id.button_reg)
+
+        button.setOnClickListener {
+            val login = userLogin.text.toString().trim()
+            val email = userEmail.text.toString().trim()
+            val pass = userPass.text.toString().trim()
+
+            if(login == "" || email == "" || pass == "")
+                Toast.makeText(this, "Здається ти щось пропустив", Toast.LENGTH_LONG).show()
+            else{
+                val user = User(login, email, pass)
+
+                val db = DbHelper(this, null)
+                db.addUser(user)
+                Toast.makeText(this, "Все $login, ти в нашій базі", Toast.LENGTH_LONG).show()
+
+                userLogin.text.clear()
+                userEmail.text.clear()
+                userPass.text.clear()
+
+            }
+        }
+
         }
     }
-}
