131de867edbf7b29ce09db1e3261b08238a3b5f7
Profesor41 <149595806+Profesor41@users.noreply.github.com>
2025-02-06 21:51:08 +0200

    have login error

--- app/src/main/java/com/example/registrationpage/ItemsActivity.kt
+++ app/src/main/java/com/example/registrationpage/ItemsActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class ItemsActivity : AppCompatActivity() {
@@ -16,8 +17,11 @@ class ItemsActivity : AppCompatActivity() {
         val items = arrayListOf<Item>()
 
         items.add(Item(1,"divan", "Диван", "штука на якій лежать", "There are a big text just to check how it works or maybe not soo big as it coukd be, anyway", 666))
-        items.add(Item(1,"TV", "То тєлік", "штука на якій дивляться щось з дивану", "There are a big text just to check how it works or maybe not soo big as it coukd be, anyway", 8888))
-        items.add(Item(1,"Xbox", "Приставка", "штука на якій грають у тєлік лежачи на дивані", "There are a big text just to check how it works or maybe not soo big as it coukd be, anyway", 99099))
+        items.add(Item(1,"tv", "То тєлік", "штука на якій дивляться щось з дивану", "There are a big text just to check how it works or maybe not soo big as it coukd be, anyway", 8888))
+        items.add(Item(1,"xbox", "Приставка", "штука на якій грають у тєлік лежачи на дивані", "There are a big text just to check how it works or maybe not soo big as it coukd be, anyway", 99099))
+
+        itemsList.layoutManager = LinearLayoutManager(this)
+        itemsList.adapter = ItemsAdapter(items, this)
 
 
         }
